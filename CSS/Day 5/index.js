7aabe56688e1e5a96d8512744fbf5df092c4779c
guptark08 <guptark08@gmail.com>
2026-04-10 14:48:44 +0530

    update

--- CSS/Day 5/index.js
+++ CSS/Day 5/index.js
@@ -33,38 +33,59 @@
 
 // console.log(cartData)   
 
-let users = [
-    {
-        id: 1,
-        username: "arush",
-        password: "password"
-    },
-    {
-        id: 2,
-        username: "Esha", 
-        password: "roshan"
-    },
-    {
-        id: 3, 
-        username: "Durgesh", 
-        password: "RandomGirl"
-    }
-]
+// let users = [
+//     {
+//         id: 1,
+//         username: "arush",
+//         password: "password"
+//     },
+//     {
+//         id: 2,
+//         username: "Esha", 
+//         password: "roshan"
+//     },
+//     {
+//         id: 3, 
+//         username: "Durgesh", 
+//         password: "RandomGirl"
+//     }
+// ]
 
-let inputData = {
-    username: "arush", 
-    password: "password"
+// let inputData = {
+//     username: "arush", 
+//     password: "password"
+// }
+
+// // let result = users.some(ele => ele.username === inputData.username && ele.password === inputData.password)
+
+// let usernData = users.find(ele => ele.username == inputData.username)
+// if(usernData){
+//     if(usernData.password === inputData.password){
+//         console.log("Login Successfull")
+//     } else {
+//         console.log("Incorrect Password")
+//     }
+// }
+
+// console.log(result);
+
+function addSpices(callback){
+    setTimeout(() => {
+        console.log("Spices Added")
+        callback()
+    }, 2000)
+}   
+
+function pourOil(callback){
+    setTimeout(() => {
+        console.log("Oil Poured")
+        callback()
+    }, 3000)    
 }
 
-// let result = users.some(ele => ele.username === inputData.username && ele.password === inputData.password)
+addSpices(() => {
+    pourOil(() => {
+        console.log("Cooking Started")
+    })}   
+)
 
-let usernData = users.find(ele => ele.username == inputData.username)
-if(usernData){
-    if(usernData.password === inputData.password){
-        console.log("Login Successfull")
-    } else {
-        console.log("Incorrect Password")
-    }
-}
-
-console.log(result);
